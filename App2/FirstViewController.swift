b9335566944f2ab4f2b85d98e69803030434819f
Léo Pichat <leo@imac-de-leo.home>
2017-12-10 16:25:17 +0100

    Initial Commit

---
+++ App2/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  App2
+//
+//  Created by Léo Pichat on 10.12.17.
+//  Copyright © 2017 Léo Pichat. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
